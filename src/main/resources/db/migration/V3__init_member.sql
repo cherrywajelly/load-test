8368b4eb23022866d3a8631c5a8066f6a16638ec
yeiin <yeini0417@gmail.com>
2025-08-26 14:51:45 +0900

    :sparkles: flyway init sql

---
+++ src/main/resources/db/migration/V3__init_member.sql
@@ -0,0 +1,54 @@
+INSERT INTO member VALUES (1,1,"creator1@email.com", "creatorProfileUrl1", "creator1", "GOOGLE", "CREATOR", "KB", "account_number","fcm_token","2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+
+INSERT INTO member VALUES (2,1,"test1@email.com", "memberProfileUrl1", "user1", "GOOGLE", "USER", "KB", "account_number","fcm_token","2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (1,2,1, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (2,2,2,"2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+
+INSERT INTO member VALUES (3,1,"test2@email.com", "memberProfileUrl2", "testNickname2", "GOOGLE", "USER", "KB", "account_number","fcm_token","2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (3,3,1, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (4,3,2,"2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+
+INSERT INTO member VALUES (4,1,"test3@email.com", "memberProfileUrl3", "testNickname3", "GOOGLE", "USER", "KB", "account_number","fcm_token","2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (5,4,1, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (6,4,2, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+
+INSERT INTO member VALUES (5,1,"test4@email.com", "memberProfileUrl4", "testNickname4", "GOOGLE", "USER", "KB", "account_number","fcm_token","2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (7,5,1, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (8,5,2, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+
+INSERT INTO member VALUES (6,1,"test5@email.com", "memberProfileUrl5", "testNickname5", "GOOGLE", "USER", "KB", "account_number","fcm_token","2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (7,6,1, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (8,6,2, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+
+INSERT INTO member VALUES (7,1,"test6@email.com", "memberProfileUrl6", "testNickname6", "GOOGLE", "USER", "KB", "account_number","fcm_token","2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (7,7,1, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (8,7,2, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+
+INSERT INTO member VALUES (8,1,"test7@email.com", "memberProfileUrl7", "testNickname7", "GOOGLE", "USER", "KB", "account_number","fcm_token","2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (7,8,1, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (8,8,2, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+
+INSERT INTO member VALUES (9,1,"test8@email.com", "memberProfileUrl8", "testNickname8", "GOOGLE", "USER", "KB", "account_number","fcm_token","2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (7,9,1, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (8,9,2, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+
+INSERT INTO member VALUES (10,1,"test9@email.com", "memberProfileUrl9", "testNickname9", "GOOGLE", "USER", "KB", "account_number","fcm_token","2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (7,10,1, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (8,10,2, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+
+INSERT INTO member VALUES (11,1,"test10@email.com", "memberProfileUrl10", "testNickname10", "GOOGLE", "USER", "KB", "account_number","fcm_token","2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (7,11,1, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (8,11,2, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+
+INSERT INTO member VALUES (12, 1,"test11@email.com", "memberProfileUrl11", "testNickname11", "GOOGLE", "USER", "KB", "account_number","fcm_token","2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (7,12,1, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (8,12,2, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+
+INSERT INTO member VALUES (13,1,"test12@email.com", "memberProfileUrl12", "testNickname12", "GOOGLE", "USER", "KB", "account_number","fcm_token","2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (7,13,1, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (8,13,2, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+
+INSERT INTO member VALUES (14,1,"test13@email.com", "memberProfileUrl13", "testNickname13", "GOOGLE", "USER", "KB", "account_number","fcm_token","2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (7,14,1, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+INSERT INTO icon_member VALUES (8,14,2, "2024-09-01 00:00:00.0", "2024-09-01 00:00:00.0");
+
