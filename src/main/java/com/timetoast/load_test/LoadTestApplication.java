fba24f4b0e43434893fd3f74aa2d40117e7a9109
yeiin <yeini0417@gmail.com>
2025-08-26 14:50:57 +0900

    :tada: init

---
+++ src/main/java/com/timetoast/load_test/LoadTestApplication.java
@@ -0,0 +1,13 @@
+package com.timetoast.load_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadTestApplication.class, args);
+	}
+
+}
